06dd7bd3f94931a32fa51e884232c91c93097195
Mark Allen <markallen.webdeveloper@gmail.com>
2020-01-24 16:45:36 +0000

    NativeBase working - setup basic app with header, footer and cards

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import { AppLoading } from 'expo';
-import { Container, Text } from 'native-base';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { AppLoading } from "expo";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text,
+  Card,
+  CardItem
+} from "native-base";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isReady: false,
+      isReady: false
     };
   }
 
   async componentDidMount() {
     await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font
     });
     this.setState({ isReady: true });
   }
@@ -28,8 +44,124 @@ export default class App extends React.Component {
 
     return (
       <Container>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Header style={styles.header}>
+          <Left style={styles.left}>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body style={styles.body}>
+            <Title>First NativeBase App</Title>
+          </Body>
+          <Right style={styles.right}></Right>
+        </Header>
+        <Content style={styles.content}>
+          <Card>
+            <CardItem header bordered>
+              <Text>NativeBase Card</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                  This is a simple card component body text which fills out
+                  information under the header included above.
+                </Text>
+              </Body>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem header bordered>
+              <Text>Links</Text>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Icon active name="logo-googleplus"  style={{color: "red"}} />
+              </Left>
+              <Body>
+                <Text>Google Plus</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Icon active name="logo-facebook" style={{color: "blue"}} />
+              </Left>
+              <Body>
+                <Text>Facebook</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Icon active name="logo-dropbox" style={{color: "blue"}} />
+              </Left>
+              <Body>
+                <Text>Dropbox</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Icon active name="logo-pinterest" style={{color: "red"}} />
+              </Left>
+              <Body>
+                <Text>Pinterest</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Icon active name="logo-snapchat" style={{color: "yellow"}} />
+              </Left>
+              <Body>
+                <Text>Snapchat</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+          </Card>
+        </Content>
+        <Footer>
+          <FooterTab style={styles.footer}>
+            <Button full>
+              <Text>Mark Allen</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  content: {
+    paddingHorizontal: 10
+  },
+  header: {
+    paddingTop: 20,
+    height: 80,
+    backgroundColor: "green"
+  },
+  body: {
+    flex: 2,
+    alignItems: "center"
+  },
+  left: {
+    flex: 1
+  },
+  right: {
+    flex: 1
+  },
+  footer: {
+    backgroundColor: "#696969"
+  }
+});
